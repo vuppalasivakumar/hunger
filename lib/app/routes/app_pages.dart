92885508d911f56880257d398a342db4f0415c22
Suresh99g <suresh.vita001@gmail.com>
2024-06-17 17:16:37 +0530

    getx structure added

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,34 @@
+import 'package:get/get.dart';
+
+import 'package:hunger/app/modules/home/bindings/home_binding.dart';
+import 'package:hunger/app/modules/home/views/home_view.dart';
+import 'package:hunger/app/modules/login/bindings/login_binding.dart';
+import 'package:hunger/app/modules/login/views/login_view.dart';
+import 'package:hunger/app/modules/sign_up/bindings/sign_up_binding.dart';
+import 'package:hunger/app/modules/sign_up/views/sign_up_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.SIGN_UP,
+      page: () => SignUpView(),
+      binding: SignUpBinding(),
+    ),
+  ];
+}
